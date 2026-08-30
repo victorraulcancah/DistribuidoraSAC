77740ac81d58d963f39de3ef25fee711fc9b291f
Victor Canchari <vcanchari38@gmail.com>
2026-08-29 20:57:26 -0500

    feat: add ClienteSeeder and ProveedorSeeder for client and supplier management

--- distribuidora/database/seeders/DatabaseSeeder.php
+++ distribuidora/database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
@@ -25,5 +24,10 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
+
+        $this->call([
+            ClienteSeeder::class,
+            ProveedorSeeder::class,
+        ]);
     }
 }
